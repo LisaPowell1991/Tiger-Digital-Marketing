6edb692c2fd4b4b0cbe673957e89f91d03b0e7be
Lisa Powell <lisapowell46@gmail.com>
2024-05-21 11:03:03 +0200

    Setup Firebase in our project

---
+++ the-shoe-spot/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB55kLo6busrve9QL4bD6FJKiJ1P0Dg8og",
+    authDomain: "the-shoe-spot-3c979.firebaseapp.com",
+    projectId: "the-shoe-spot-3c979",
+    storageBucket: "the-shoe-spot-3c979.appspot.com",
+    messagingSenderId: "576331912544",
+    appId: "1:576331912544:web:a3470b6f290cd0fbac3a05"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
