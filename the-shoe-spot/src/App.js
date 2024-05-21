7afd2875c40130b3cce8089d5bd9a3661e6abdf8
Lisa Powell <lisapowell46@gmail.com>
2024-05-21 19:14:33 +0200

    Login/SignIn and Logout functionality

--- the-shoe-spot/src/App.js
+++ the-shoe-spot/src/App.js
@@ -1,11 +1,43 @@
-// src/App.js
-import React from 'react';
-import AboutUs from '../src/components/About';
+import React, { useState, useEffect } from 'react';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import { auth } from './config/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import AboutUs from './components/About';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
-    <div>
-      <AboutUs />
+    <div className="App">
+      {user ? (
+        <div>
+          <h1>Welcome, {user.email}</h1>
+          <Logout />
+        </div>
+      ) : (
+        <div>
+          <h1>Sign Up</h1>
+          <Signup />
+          <h1>Login</h1>
+          <Login />
+        </div>
+      )}
+
+      <div>
+        <AboutUs />
+      </div>
     </div>
   );
 }
