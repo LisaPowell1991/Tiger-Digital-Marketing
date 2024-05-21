21f812584566dec9b70b7d8d3d4f9ef0d1ab67da
Marley <122859023+Marley-Enoghe@users.noreply.github.com>
2024-05-21 21:07:38 +0100

    Created The header-Component

---
+++ the-shoe-spot/src/components/header/header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Logo from './Assets/Shoe_spot.png';
+import './header.css'
+
+const Headers= () => {
+  return (
+    <header>
+      <div className="logo">
+        <a>
+          <img src={Logo} />
+        </a>
+      </div>
+      <div className="links">
+        <ul>
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Products</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+          <li>
+            <a>Cart</a>
+          </li>
+          <li>
+            <a>Checkout</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+export default Headers;
