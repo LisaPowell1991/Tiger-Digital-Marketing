c2b041a98b0878e60d60f035fd5865f3216dd273
Lisa Powell <lisapowell46@gmail.com>
2024-05-21 11:48:06 +0200

    created a .env file to store all the Firebase config values for security reasons

--- the-shoe-spot/src/config/firebase.js
+++ the-shoe-spot/src/config/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB55kLo6busrve9QL4bD6FJKiJ1P0Dg8og",
-    authDomain: "the-shoe-spot-3c979.firebaseapp.com",
-    projectId: "the-shoe-spot-3c979",
-    storageBucket: "the-shoe-spot-3c979.appspot.com",
-    messagingSenderId: "576331912544",
-    appId: "1:576331912544:web:a3470b6f290cd0fbac3a05"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication
+const auth = getAuth(app);
+
+export { app, auth };
