606e0588bc6fd686de87f81fc6ab51cfa7fdee44
Lisa Powell <lisapowell46@gmail.com>
2024-05-29 17:08:22 +0200

    Created stripe integration so that payment goes through successfully

--- the-shoe-spot/src/App.js
+++ the-shoe-spot/src/App.js
@@ -4,34 +4,44 @@ import AboutUs from "./components/About";
 import Headers from "./components/header/header";
 import Signup from './components/Signup';
 import Login from './components/Login';
-import Logout from './components/Logout';
 import Footer from './components/footer/footer';
 import Contact from './components/contact/contact';
 import ProductPage from './components/ProductPage';
 import ProductDetail from './components/ProductDetail';
+import CheckoutForm from './components/CheckoutForm';
 import { auth } from './config/firebase';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from './config/firebase';
-import { Button } from 'react-bootstrap';
+import { Elements } from '@stripe/react-stripe-js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { stripePromise } from './config/firebase';
+import CheckoutPage from './components/Cart';
 
 function App() {
   const [user, setUser] = useState(null);
   const [showLogin, setShowLogin] = useState(false);
   const [showSignup, setShowSignup] = useState(false);
   const [shoes, setShoes] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    console.log("kom ons in die addtocart?")
+    setCart(prevCart => [...prevCart, item]);
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
     });
 
+    console.log("cart:::", cart)
+
     return () => {
       unsubscribe();
     };
-  }, []);
+  }, [cart]);
 
   useEffect(() => {
     const fetchShoes = async () => {
@@ -77,11 +87,17 @@ function App() {
         <Signup show={showSignup} handleClose={handleCloseSignup} />
 
         <Routes>
-          <Route path="/" element={<ProductPage shoes={shoes} />} />
+          <Route path="/home" element={<ProductPage shoes={shoes} />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/products" element={<ProductPage shoes={shoes} />} />
-          <Route path="/product/:id" element={<ProductDetail shoes={shoes} />} />
+          <Route path="/product/:id" element={<ProductDetail shoes={shoes} addToCart={addToCart} />} />
+          <Route path="/cart" element={<CheckoutPage cart={cart} />} />
+          <Route path="/checkout" element={
+            <Elements stripe={stripePromise}>
+              <CheckoutForm cart={cart} />
+            </Elements>
+          } />
         </Routes>
 
         <Footer />
